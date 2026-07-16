0b0704d1f344c5d762dfb74fe8e9a34084d7d671
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-07-16 12:40:48 +0700

    unit test genpass

---
+++ internal/service/genpass_test.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePassword(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name string
+
+		expectedLength int
+		expectedError  error
+	}{
+		{
+			name:           "success",
+			expectedLength: 12,
+			expectedError:  nil,
+		},
+		{
+			name:           "success with custom length",
+			expectedLength: 1,
+			expectedError:  nil,
+		},
+		{
+			name:           "success with custom length",
+			expectedLength: 10000,
+			expectedError:  nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			testService := NewGenPass()
+			password, err := testService.GeneratePassword(tc.expectedLength)
+
+			assert.ErrorIs(t, err, tc.expectedError)
+			assert.Equal(t, tc.expectedLength, len(password))
+		})
+	}
+}
