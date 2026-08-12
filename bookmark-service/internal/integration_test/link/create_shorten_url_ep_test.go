55be6a2f327cab8786381e8e56dd796aab6dbe16
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-08-12 15:11:21 +0700

    sonarqube duplicate code

--- bookmark-service/internal/integration_test/link/create_shorten_url_ep_test.go
+++ bookmark-service/internal/integration_test/link/create_shorten_url_ep_test.go
@@ -60,15 +60,12 @@ func TestLinkEndpoint_CreateShortenUrl(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			setupRedisClient := redisPkg.InitMockRedis(t)
-			setupDB := sqldb.InitMockDB(t)
-			testAPI := api.NewEngine(&api.EngineOpts{
+			recorder := tc.setupTestHTTP(api.NewEngine(&api.EngineOpts{
 				App:         gin.Default(),
 				Cfg:         &api.Config{},
-				RedisClient: setupRedisClient,
-				DbClient:    setupDB,
-			})
-			recorder := tc.setupTestHTTP(testAPI)
+				RedisClient: redisPkg.InitMockRedis(t),
+				DbClient:    sqldb.InitMockDB(t),
+			}))
 
 			assert.Equal(t, tc.expectedStatusCode, recorder.Code)
 			assert.Contains(t, recorder.Body.String(), tc.expectedResponseBody)
