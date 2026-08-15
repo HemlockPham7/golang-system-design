71080d36452bffe939087be4d4ef8f4957225332
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-08-15 12:12:14 +0700

    compile error

--- bookmark-service/cmd/worker/main.go
+++ bookmark-service/cmd/worker/main.go
@@ -5,8 +5,11 @@ import (
 
 	workerHdl "github.com/HemlockPham7/golang-system-design/internal/handler/worker"
 	"github.com/HemlockPham7/golang-system-design/internal/infrastructure"
+	bookmarkRepo "github.com/HemlockPham7/golang-system-design/internal/repository/bookmark"
 	"github.com/HemlockPham7/golang-system-design/internal/repository/queue"
+	"github.com/HemlockPham7/golang-system-design/internal/service/bookmark"
 	workerEng "github.com/HemlockPham7/golang-system-design/internal/worker"
+	"github.com/HemlockPham7/golang-system-design/pkg/utils"
 )
 
 const queueName = "bookmark-queue"
@@ -16,8 +19,14 @@ func main() {
 	redisClient := infrastructure.CreateRedisClient("")
 	queueRepository := queue.NewRedisQueue(redisClient, queueName)
 
+	// db
+	dbClient := infrastructure.CreateDB("")
+
 	// handler
-	handler := workerHdl.NewHandler()
+	bookmarkRepository := bookmarkRepo.NewRepository(dbClient)
+	codeGen := utils.NewGenPass()
+	bookmarkService := bookmark.NewService(bookmarkRepository, codeGen)
+	handler := workerHdl.NewHandler(bookmarkService)
 
 	// engine
 	engine := workerEng.NewEngine(queueRepository, handler)
